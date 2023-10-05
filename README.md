d99fb0ef8b0f175f0278e6566e1118aa85b86577
attilamonteiro <atmaload@gmail.com>
2023-10-05 10:40:25 +0000

    modulo27

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# -curso_ebac_frontend
+Vue.js Calculadora Aritmética
+Este é um projeto simples de calculadora aritmética desenvolvido usando Vue.js. Ele fornece uma interface intuitiva para realizar operações de adição, subtração, multiplicação e divisão em tempo real.
+
+Pré-requisitos
+Node.js e npm instalados
+Vue CLI instalado globalmente (npm install -g @vue/cli)
+Como Executar o Projeto Localmente
+Clone o Repositório:
+
+git clone https://github.com/seu-usuario/vue-calculadora.git
+cd vue-calculadora
+
+Instale as Dependências:
+
+npm install
+Execute o Projeto:
+
+npm run serve
+O aplicativo estará disponível em http://localhost:8080.
+
+Funcionalidades
+
+Dois campos para inserir números.
+Um campo do tipo select para escolher a operação aritmética.
+O cálculo é realizado automaticamente ao alterar os valores, sem a necessidade de clicar em nenhum botão.
+
+Tecnologias Utilizadas
+Vue.js
+Bootstrap e Bootstrap Vue para estilização
+
+Estrutura do Projeto
+src/App.vue: Componente principal que contém a lógica da calculadora.
+src/main.js: Ponto de entrada do aplicativo.
+
+Personalização
+
+Os estilos podem ser personalizados editando o arquivo src/App.vue.
+Adicione mais operações ou recursos conforme necessário.
+
+Contribuindo
+Sinta-se à vontade para contribuir, abrir problemas ou sugerir melhorias. Toda contribuição é bem-vinda!
---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app" class="container mt-5">
+    <div class="row">
+      <div class="col-md-4 offset-md-4">
+        <h2>Calculadora Aritmética</h2>
+        <div class="form-group">
+          <label for="num1">Número 1:</label>
+          <input v-model="num1" type="number" class="form-control" @input="calcularResultado" />
+        </div>
+        <div class="form-group">
+          <label for="num2">Número 2:</label>
+          <input v-model="num2" type="number" class="form-control" @input="calcularResultado" />
+        </div>
+        <div class="form-group">
+          <label for="operacao">Operação:</label>
+          <select v-model="operacao" class="form-control" @change="calcularResultado">
+            <option value="+">Soma</option>
+            <option value="-">Subtração</option>
+            <option value="*">Multiplicação</option>
+            <option value="/">Divisão</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="resultado">Resultado:</label>
+          <input :value="resultado" type="text" class="form-control" disabled />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.num1 + this.num2;
+        case '-':
+          return this.num1 - this.num2;
+        case '*':
+          return this.num1 * this.num2;
+        case '/':
+          return this.num1 / this.num2;
+        default:
+          return 0;
+      }
+    },
+  },
+  methods: {
+    calcularResultado() {
+      // Os resultados serão atualizados automaticamente devido ao uso de v-model
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+}
+
+input[disabled] {
+  background-color: #e9ecef;
+}
+
+</style>
+
